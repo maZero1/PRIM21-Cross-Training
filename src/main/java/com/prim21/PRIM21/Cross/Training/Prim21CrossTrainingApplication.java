ed8302ea71824419bf57a37ef64db8c2b95fb44f
maZero1 <tkmarlon594@gmail.com>
2025-11-24 17:40:03 -0300

    chore: estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/prim21/PRIM21/Cross/Training/Prim21CrossTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.prim21.PRIM21.Cross.Training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prim21CrossTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prim21CrossTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prim21/PRIM21/Cross/Training/Prim21CrossTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prim21.PRIM21.Cross.Training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prim21CrossTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
